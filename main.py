9cd081f5529f068b76446f7eda0994180775603e
Segfault (Core dumped) <tyiwei0@gmail.com>
2025-03-03 10:16:25 +0800

    Changed selected country

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app: Flask = Flask(__name__)
@@ -7,22 +7,37 @@ app: Flask = Flask(__name__)
 @app.route("/", methods=["POST", "GET"])
 def index() -> str:
     db = sqlite3.connect("db/database.db")
-    countries = tuple(db.execute("select name from countries order by id asc"))
+    countries = tuple(
+        db.execute("select name, country_code from countries order by id asc")
+    )
     db.close()
 
-    countries = [country[0] for country in countries]
+    country_data = dict([(country[0], country[1]) for country in countries])
 
     if request.method == "GET":
-        return render_template("index.html", countries=countries)
+        return render_template(
+            "index.html", countries=[country for country in country_data.keys()]
+        )
 
     if request.method == "POST":
-        country = request.form["Country"]
+        selected_country = request.form["Country"]
 
-        if country == "Select country":
-            return render_template("index.html", countries=countries)
+        if selected_country not in country_data.keys():
+            pass
 
-        arcades = ["Virtualand", "Timezone"]
-        return render_template("index.html", countries=countries, arcades=arcades)
+        if selected_country == "Select country":
+            render_template(
+                "index.html",
+                countries=[country for country in country_data.keys()],
+                selected_country="",
+            )
+
+        return redirect(url_for(f"country", selected_country=selected_country))
+
+
+@app.route("/<selected_country>/")
+def country(selected_country: str):
+    return render_template("country.html", selected_country=selected_country)
 
 
 if __name__ == "__main__":
